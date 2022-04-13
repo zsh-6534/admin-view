c9bf3e9f39c9306fe06e83166e50a99e6b108cef
zhoushanghai <861172793@qq.com>
2022-04-13 17:07:08 +0800

    admin-view++

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 引入 element-ui
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/zh-CN";
+Vue.use(ElementUI, { locale });
+
 Vue.config.productionTip = false;
 
 new Vue({
